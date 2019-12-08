bfef4808eedabf5ba89180cdb6a9af32a90035c3
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 09:50:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -25,3 +25,11 @@ def map_to_square(array_of_numbers)
   end
   myNewArray
 end
+
+def map_to_square(array_of_numbers)
+  myNewArray = []
+  array_of_numbers.each do |myElement|
+    myNewArray.push(myElement * myElement)
+  end
+  myNewArray
+end
