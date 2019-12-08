bbcd1cc830956714ae554e84ef068704352371ab
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 10:04:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -26,16 +26,8 @@ def map_to_square(array_of_numbers)
   myNewArray
 end
 
-def reduce_to_total(array_of_numbers)
-  myTotal = 0
-  array_of_numbers.each do |myElement|
-    myTotal = myTotal + myElement
-  end
-  myTotal
-end
-
 def reduce_to_total(array_of_numbers, startingPoint)
-  myTotal = startingPoint
+  myTotal = startingPoint ? startingPoint : 0
   array_of_numbers.each do |myElement|
     myTotal = myTotal + myElement
   end
