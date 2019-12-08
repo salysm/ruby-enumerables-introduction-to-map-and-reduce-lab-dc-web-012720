bb6050d4343cd9bf672c4b1fd7b4cbded619eedd
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 10:08:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -26,8 +26,16 @@ def map_to_square(array_of_numbers)
   myNewArray
 end
 
+def reduce_to_total(array_of_numbers)
+  myTotal = 0
+  array_of_numbers.each do |myElement|
+    myTotal = myTotal + myElement
+  end
+  myTotal
+end
+
 def reduce_to_total(array_of_numbers, startingPoint)
-  puts "HERERERERE " + startingPoint
+  puts startingPoint
   myTotal = startingPoint ? startingPoint : 0
   array_of_numbers.each do |myElement|
     myTotal = myTotal + myElement
