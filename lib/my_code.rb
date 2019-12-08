7dfe40c4341a1e12feab7d050e76cb1578a80f34
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 09:59:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,9 +45,7 @@ end
 def reduce_to_all_true(array_of_values)
   theTruthy = true
   array_of_values.each do |myElement|
-    if(!myElement){
-      theTruthy = false
-    }
+    theTruthy = myElement ?
   end
   theTruthy
 end
