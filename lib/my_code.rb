e1f46af7c88002767e51be581639ebf9e85d0776
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-12-08 09:49:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,10 +18,10 @@ def map_to_double(array_of_numbers)
   myNewArray
 end
 
-def map_to_double(array_of_numbers)
+def map_to_square(array_of_numbers)
   myNewArray = []
   array_of_numbers.each do |myElement|
-    myNewArray.push(myElement * 2)
+    myNewArray.push(myElement * myElement)
   end
   myNewArray
 end
